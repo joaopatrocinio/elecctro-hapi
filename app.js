81bdc4e807c77f60259502f218dd32f27a4197af
João Patrocínio <jpatrocinio5436@hotmail.com>
2021-08-06 21:54:20 +0100

    Project setup

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    const swaggerOptions = {
+        info: {
+            title: 'Elecctro Server Challenge',
+            version: Pack.version
+        }
+    };
+
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
